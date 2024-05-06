44bbf11e484a502df6e65df352b3e66cf6f58a3f
Rafael Villegas <rafa.villegas.michel@gmail.com>
2024-05-06 11:53:10 -0500

    Add bartib alias

--- alias.zsh
+++ alias.zsh
@@ -1,3 +1,11 @@
+# eza
 alias l="eza -l -h -a --icons --group-directories-first"
 
+# lazygit
 alias lg="lazygit"
+
+# bartib
+alias bl="bartib list --today"
+alias bll="bartib list"
+alias br="bartib report --today"
+alias brr="bartib report"
