31dbe18da442aee711cd9a05bc97354ebeb5977a
Rafael Villegas <rafa.villegas.michel@gmail.com>
2025-04-07 10:13:43 -0500

    Add astronvim v5 config

---
+++ nvim/lua/community.lua
@@ -0,0 +1,42 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.lua" },
+  -- import/override with your plugins folder
+
+  -- Programming languages
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.go" },
+
+  -- Config languages
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.markdown" },
+
+  -- Dev tools
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.terraform" },
+
+  -- DB support
+  { import = "astrocommunity.pack.full-dadbod" },
+
+  -- AI tools
+  { import = "astrocommunity.completion.avante-nvim" },
+
+  -- Motion
+  { import = "astrocommunity.motion.mini-surround" },
+
+  -- Completion
+  { import = "astrocommunity.completion.copilot-cmp" },
+
+  -- Colorscheme
+  { import = "astrocommunity.colorscheme.catppuccin" },
+}
---
+++ nvim/lua/plugins/smart-splits.lua
@@ -0,0 +1,15 @@
+return {
+  "mrjones2014/smart-splits.nvim",
+  lazy = false,
+  opts = {
+    ignored_filetypes = {
+      "nofile",
+      "quickfix",
+      "qf",
+      "prompt",
+    },
+    ignored_buftypes = {
+      "nofile",
+    },
+  },
+}
