1e787984aca2e7d5b77b13f0f3ec6ce6fd6ee2be
Rafael Villegas <rafa.villegas.michel@gmail.com>
2025-04-14 14:13:13 -0500

    Add custom nvim dashboard

--- nvim/lua/plugins/astrocore.lua
+++ nvim/lua/plugins/astrocore.lua
@@ -83,10 +83,6 @@ return {
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
       },
-      i = {
-        -- Remap 'jk' to 'Esc' in insert mode
-        ["jk"] = { "<Esc>", desc = "Exit insert mode" },
-      },
     },
   },
 }
