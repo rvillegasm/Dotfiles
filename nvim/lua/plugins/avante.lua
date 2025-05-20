08396de990adc5e3e9c36711b2923c4f4f2d066b
Rafael Villegas <rafa.villegas.michel@gmail.com>
2025-05-20 11:21:40 -0500

    Update nvim and avante to use 3.7 thought

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -2,7 +2,7 @@ return {
   "yetone/avante.nvim",
   opts = {
     copilot = {
-      model = "claude-3.7-sonnet",
+      model = "claude-3.7-sonnet-thought",
     },
     windows = {
       width = 35,
